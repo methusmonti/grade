7b187250901b463ffdca70d42bbf683a67e9e58a
methusmonti <61682353+methusmonti@users.noreply.github.com>
2020-03-03 13:35:47 +0700

    Add function - input value by console

--- src/com/montivory/grade/Main.java
+++ src/com/montivory/grade/Main.java
@@ -1,11 +1,22 @@
 package com.montivory.grade;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Grade g = new Grade();
 
-        int score = 50;
+        System.out.print("Enter a Score: ");
+
+        // input score
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input = reader.readLine();
+
+        // parse score to integer
+        int score = Integer.parseInt(input);
 
         // calculate result
         String result = g.calculateGrade(score);
