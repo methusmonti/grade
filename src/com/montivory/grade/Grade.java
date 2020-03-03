3a32c1954d98f251f3a61a763fea1b3904bea6ac
methusmonti <61682353+methusmonti@users.noreply.github.com>
2020-03-03 13:31:53 +0700

    Create Grade class and Calculate function

---
+++ src/com/montivory/grade/Grade.java
@@ -0,0 +1,20 @@
+package com.montivory.grade;
+
+public class Grade {
+    public String calculateGrade(int point) {
+        if(point >= 80) {
+            return "A";
+        }
+        else if (point >= 70) {
+            return "B";
+        }
+        else if (point >= 60) {
+            return "C";
+        }
+        else if (point >= 50) {
+            return "D";
+        }
+        else
+            return "F";
+    }
+}
