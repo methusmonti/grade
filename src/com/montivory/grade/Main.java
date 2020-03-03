75ad4379034fbf0ef562e7825be86170790d12c2
methusmonti <61682353+methusmonti@users.noreply.github.com>
2020-03-03 13:33:48 +0700

    Create basic main code using static input

--- src/com/montivory/grade/Main.java
+++ src/com/montivory/grade/Main.java
@@ -3,6 +3,14 @@ package com.montivory.grade;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Grade g = new Grade();
+
+        int score = 50;
+
+        // calculate result
+        String result = g.calculateGrade(score);
+
+        // display result
+        System.out.println("Score = "+score+" | Grade = "+result);
     }
 }
